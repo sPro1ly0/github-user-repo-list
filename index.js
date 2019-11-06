7f87e7c834a86879a130dd48637d941a2f6000c2
Sabrina Becker <sbecka8@gmail.com>
2019-11-06 14:33:28 -0500

    app is working, all code present

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 'use strict';
+
+//user should input a user handle
+//catch any errors in submitted input value
+//call github api
+//display repo name and repo URL link
+//user can make multiple searches and see only results from current search
+
+function getUrl() {
+
+    let userInput = $("#user-handle").val();
+
+    let gitHubURL = `https://api.github.com/users/${userInput}/repos?sort=created`;
+    console.log(gitHubURL);
+
+    fetch(gitHubURL)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error (response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => {
+            $(".error-message").text(`User ${userInput} Is ${error.message}.`)
+        });
+};
+
+function displayResults(responseJson) {
+
+   console.log(responseJson); 
+    $(".repos").empty();
+    $(".error-message").empty();
+
+    for (let i = 0; i < responseJson.length; i++) {
+        $(".repos").append(`
+        <li>${responseJson[i].name}: <a href = "${responseJson[i].html_url}">${responseJson[i].html_url}</a></li>`);
+    };
+
+    $(".results").removeClass("hidden");
+};
+
+function gitHubHandleForm() {
+    $("form").submit( event => {
+        event.preventDefault();
+        getUrl();
+        $("#user-handle").val("");
+    });
+};
+
+$(gitHubHandleForm);
